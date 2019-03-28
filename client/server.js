48f97b96dc2f421940925624e1cdd9025d615052
weloper <sen.webdev@hotmail.com>
2019-03-28 08:28:24 -0700

    hj

--- client/server.js
+++ client/server.js
@@ -2,9 +2,11 @@ var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
 
+console.log('server.js: custom message');
+
 var app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
 
-var port = process.env.PORT || 8000
+var port = process.env.PORT || 8080
 app.listen(port)
 console.log('server started ' + port)
